ad068999a7d8f775c4422a071006cff3090a9b4b
Marisha <7513541@gmail.com>
2023-06-11 00:22:52 +0300

    style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, DoCheck, OnInit } from '@angular/core';
+import { Component, Input, DoCheck, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { find, Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
@@ -13,11 +13,17 @@ import { RoleUsers } from 'src/app/shared/types/role-users.enum';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   @Input() public progressValue: number = 0;
+
+  private progressBarWidth = 0;
+  private first!: Element | null;
+  private last!: Element | null;
+  private star!: HTMLImageElement | null;
 
   public user$ = this.userService.user$;
   public userRoles!: RoleUsers;
+  public starLeft = 0;
 
   public get roleUsers(): typeof RoleUsers {
     return RoleUsers;
@@ -31,17 +37,34 @@ export class HeaderComponent implements OnInit {
     private changeProgressService: ChangeProgressService,
     private router: Router,
     private authService: AuthService,
-    private forwards: ForwardsService,
-    private activatedRoute: ActivatedRoute,
     private userService: UserService,
   ) { }
 
   ngOnInit(): void {
+    this.first = document.querySelector('#first');
+    this.last = document.querySelector('#last');
+    this.star = document.querySelector('.star');//потом будут проблемы с полосой скролла
 
+    const progressBar = document.querySelector('.header-progress')!;
+    this.progressBarWidth = progressBar?.clientWidth
   }
 
-  changeProgress(event: any): number {
-    this.progressValue = this.changeProgressService.changeProgress(event);
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const active = document.querySelector('.progress-link.active');
+      if (active) {
+        this.changeProgress({ target: active as HTMLElement } as unknown as Event)
+      }
+    }, 100);
+  }
+
+  changeProgress(event: Event): number {
+    const target = event.target as HTMLElement;
+    const progress = this.changeProgressService.changeProgress(event);
+    this.progressValue = progress / this.progressBarWidth * 100
+    const left = progress
+
+    this.starLeft = left;
     return this.progressValue;
   }
 
--- src/app/shared/services/change-progress.service.ts
+++ src/app/shared/services/change-progress.service.ts
@@ -6,26 +6,23 @@ import { Injectable, Input } from '@angular/core';
 export  class ChangeProgressService {
 
   private progressBar!: Element | null;
-  private first!: Element | null;
-  private last!: Element | null;
-  private star!: HTMLImageElement | null;
+  
   private progressValue: number = 0;
 
   constructor() { }
 
- changeProgress(event: any): number {
-    this.first = document.querySelector('#first');
-    this.last = document.querySelector('#last');
-    this.progressBar = document.querySelector('.header-progress');
-    this.star = document.querySelector('.star');//потом будут проблемы с полосой скролла
-    event.target === this.first ? this.progressValue = 0 : this.progressValue = Math.abs(((((window.innerWidth - this.progressBar!.clientWidth)/2 - event.target.offsetLeft) - this.progressBar!.clientWidth/4) - event.target.clientWidth/2 ) + this.progressBar!.clientWidth/4)*0.25;
-    if(event.target === this.last){
-      this.star!.style.left = 400 - this.star!.clientWidth/2 + "px";
-      this.progressValue = 100
-    } else {
-      this.star!.style.left = this.progressValue*4 - this.star!.clientWidth/2 + "px";
-    }
+ changeProgress(event: Event): number {
+    const target = event.target as HTMLElement
 
-    return this.progressValue;
+    const left = this.getLinkCenterX(target)
+    return left;
+  }
+
+  private getLinkHalfWidth(link: HTMLElement) {
+    return link.clientWidth / 2;
+  }
+
+  private getLinkCenterX(link: HTMLElement): number {
+    return link.offsetLeft + this.getLinkHalfWidth(link)
   }
 }
